d33a36196255ebf6863d15f2bbe37f670467d1c2
Romain Francois <romain@purrple.cat>
2024-02-03 11:04:18 +0100

    + peek()

--- R/peek.R
+++ R/peek.R
@@ -1,9 +1,28 @@
+#' @export
+informant <- function() {
+  env <- parent.frame()
+  calls <- sys.calls()
+
+  pink <- cli::make_ansi_style("pink")
+  call <- deparse(calls[[length(calls) - 5L]])
+
+  print(cli::rule(col = "pink"))
+  cat(pink("⬢"), cli::col_silver("call: "), call, "\n")
+
+  print(cli::rule(col = "pink"))
+  cat(pink("⬢"), cli::col_silver("env: \n"))
+  print(ls.str(env))
+  print(cli::rule(col = "pink"))
+  invisible()
+}
+
 #' Peek function inputs
 #'
 #' @examples
 #' peek()
 #'
 #' @export
-peek <- function() {
-  cat("hello\n")
+peek <- function(fun) {
+  fun <- substitute(fun)
+  trace(fun, tracer = snitch::informant)
 }
